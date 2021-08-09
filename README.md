aa8d44b70f710020e88be133afb3c7c3750b725c
thanhtunguet <thanhtung.uet@gmail.com>
2021-08-09 10:51:52 +0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-react3l-native-kit
+
+React3L Kit for React Native
+
+## Installation
+
+```sh
+npm install react-native-react3l-native-kit
+```
+
+## Usage
+
+```js
+import React3lNativeKit from "react-native-react3l-native-kit";
+
+// ...
+
+const result = await React3lNativeKit.multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativereact3lnativekit/React3lNativeKitPackage.kt
@@ -0,0 +1,17 @@
+package com.reactnativereact3lnativekit
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class React3lNativeKitPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return emptyList()
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return listOf(React3lNativeKitViewManager())
+    }
+}
