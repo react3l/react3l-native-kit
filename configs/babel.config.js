ab70d4a3720017a1a441b96700bbfe25ca4c98c4
thanhtunguet <thanhtung.uet@gmail.com>
2022-04-20 18:16:43 +0700

    add config files

---
+++ configs/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          'assets': './assets',
+          'src': './src',
+          'app.json': './app.json',
+          'package.json': './package.json',
+        },
+      },
+    ],
+    ['macros'],
+    ['inline-dotenv'],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+  ],
+};
